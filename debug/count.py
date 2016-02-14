cec536758085b0bbc70346311a140326c607e5fa
oliverlew <oliver_lew@outlook.com>
2016-02-14 22:15:58 +0800

    some fixes

--- debug/count.py
+++ debug/count.py
@@ -2,7 +2,7 @@
 # count the words of files in the current project and write to a file
 import datetime, os, sys
 
-num = os.popen('cat * 2> /dev/null | wc').read().split()
+num = os.popen('cat ./include/* ./src/* 2> /dev/null | wc').read().split()
 
 file = open(sys.path[0] + '/count','a')
 file.write(datetime.datetime.today().ctime())
