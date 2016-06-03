5b1ca6759a4ceb2238610505c39831189744c534
OliverLew <oliver_lew@outlook.com>
2016-06-03 20:20:24 +0800

    add documentation

--- LibFunction/LibFunction.c
+++ LibFunction/LibFunction.c
@@ -2,11 +2,14 @@
 #include <stdio.h>
 #include "NR.h"
 #undef malloc
+/**
+ * @brief safer malloc function with bland pointer check
+ */
 void* malloc_s(size_t size)
 {
     if(size < 0)
     {
-        fprintf(stderr, "allocated space size is negative\n");
+        fprintf(stderr, "malloc: allocated space size is negative\n");
         exit(1);
     }
     void* new_mem = malloc(size);
@@ -19,11 +22,14 @@ void* malloc_s(size_t size)
 }
 
 #undef realloc
+/**
+ * @brief safer realloc function with bland pointer check
+ */
 void* realloc_s(void* ptr, size_t size)
 {
     if(size < 0)
     {
-        fprintf(stderr, "allocated space size is negative\n");
+        fprintf(stderr, "realloc: allocated space size is negative\n");
         exit(1);
     }
     void* new_mem = realloc(ptr, size);
