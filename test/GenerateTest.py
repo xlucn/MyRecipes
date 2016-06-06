8d02cbb2d19b7775498820532a419985dc4637c5
OliverLew <oliver_lew@outlook.com>
2016-06-07 00:34:50 +0800

    add comments

--- test/GenerateTest.py
+++ test/GenerateTest.py
@@ -4,7 +4,6 @@ Author: Lu Xu
 Generate the header files including all the declarations of test functions,
 a array of functions, a array of function names and the number of functions.
 '''
-
 import os
 
 testh = 'Test.h'
@@ -15,7 +14,9 @@ def readfuncs(filename):
     '''
     read files and find test function declarations or definitions.
     return a list of sorted function names.
+    invoke linux command here, so it is not goint to work under Windows.
     '''
+    # TODO: change to platform-independent
     regex = 'int +test[1-9|a-z|A-Z|_]* *\( *\)*'
     command = "awk '/%s/{print$2}' %s|sort|awk -F'(' '{print $1}'"  % (regex, filename)
     return os.popen(command).read().split()
@@ -34,7 +35,8 @@ def gentest(funcs):
  * the related lines in the makefile.
  */
 '''
-    headercontent = description +
+    # The header file contains the declarations of all the test functions
+    headercontent = description + \
 '''
 #ifndef _TESTDECLARATION_H_
 #define _TESTDECLARATION_H_
@@ -52,7 +54,9 @@ void testall();
 
 #endif
 ''' % ''.join(['int %s();\n' % func for func in funcs])
-    cfilecontent = description +
+    # the source file contains the definition of the function array and name
+    # string array of all test functions and the number of test functions
+    cfilecontent = description + \
 '''
 #include "Test.h"
 
@@ -65,15 +69,21 @@ char *names[] = {
 };
 
 int num = %d;
-''' %  (''.join(['\t%s,\n' % i for i in funcs])[:-2],
-        ''.join(['\t\"%s\",\n' % i for i in funcs])[:-2],
+''' %  (''.join(['\t%s,\n' % func for func in funcs]),
+        ''.join(['\t\"%s\",\n' % func for func in funcs]),
         len(funcs))
+    # write into files
     with open(testh, 'w') as header:
         header.write(headercontent)
     with open(testinfo, 'w') as cfile:
         cfile.write(cfilecontent)
 
 def main():
+    '''
+    If the header file exists and the functions in it are the same as those in
+    source file 'Test.c', then it is up to date. Otherwise update the header
+    file and another source file.
+    '''
     funclist = readfuncs(testc)
     if os.path.exists(testh) and funclist == readfuncs(testh):
         print 'File ' + testh + ' and ' + testinfo + ' are up to date.'
