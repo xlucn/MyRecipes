78d841467e7c2084c825a20a7182881c955241bf
OliverLew <oliver_lew@outlook.com>
2016-06-12 22:12:02 +0800

    improve comment and output

--- test/Test.c
+++ test/Test.c
@@ -4,11 +4,6 @@
  * @brief test functions
  * @note if a function is declared as "int test...()", it will be collected by
  *       GenerateTest.py and regarded as a test function.
- * This testall() function will test all the functions in this project. The list
- * of the test functions are extracted from functions below in this file matching
- * the pattern "int test...()". Each test function will test one function for a
- * perticular algorithm, returning PASSED(0) or FAILED(1). At last, a report will
- * be printed after everything is finished.
  */
 
 #include <stdio.h>
@@ -16,12 +11,19 @@
 
 /**
  * @brief Test all the functions in this project
+ * 
+ * This testall() function will test all the functions in this project. The test
+ * functions list is extracted from other source files in this folder. All the
+ * function names matching the pattern "int test...()" will be included. Each 
+ * test function will test one function for a perticular algorithm, returning 
+ * PASSED(0) or FAILED(1). At last, a report will be printed after everything 
+ * is finished.
  */
 void testall()
 {
-    /* "FUNC_COUNT" defined in "Test.h": the number of test functions. */
-    /* "FUNC_ARRAY" defined in "Test.h": an array of test functions.*/
-    /* "NAME_ARRAY" defined in "Test.h": an array of test functions' names.*/
+    /** "FUNC_COUNT" defined in "Test.h": the number of test functions. */
+    /** "FUNC_ARRAY" defined in "Test.h": an array of test functions.*/
+    /** "NAME_ARRAY" defined in "Test.h": an array of test functions' names.*/
     int (*tests[FUNC_COUNT])() = FUNC_ARRAY;
     char *names[FUNC_COUNT] = NAME_ARRAY;
     int result[FUNC_COUNT];
@@ -36,9 +38,10 @@ void testall()
     }
 
 	/* summary */
+    
     for (int i = 0; i < FUNC_COUNT; i++)
     {
-        printf("%s No.%02d %s\n", result[i] ? "*[failed]" : " [passed]", i, names[i]);
+        printf("%s No.%02d %s\n", (result[i] == PASSED) ? "[passed]" : "\033[31m[failed]\033[0m", i, names[i]);
     }
 }
 
