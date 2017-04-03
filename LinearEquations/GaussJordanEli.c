4f201868f4b5d9379d5efcc34698acf25621ea25
OliverLew <oliver_lew@outlook.com>
2017-04-03 19:28:35 +0800

    fixed errors in alg

--- LinearEquations/GaussJordanEli.c
+++ LinearEquations/GaussJordanEli.c
@@ -1,60 +1,50 @@
 #include <math.h>
 #include "NR.h"
+#include "NRprivate.h"
+#include "constants.h"
 
 /**
  * @brief Gauss Jordan elimination method to solve a system of linear equations
  * @param N the rank of the equation
- * @param a the augmented matrix
+ * @param A the coefficient matrix in 1d form
+ * @param b the constant vector
+ * @returns An array of numbers, NULL if the equation has no solution
  */
 double *GaussJordanEli(int N, double *A, double *b)
 {
     double **Ab = AugmentedMatrix(A, b, N, N, 1);
-    int* max = (int*)malloc_s(N * sizeof(int));
-    double* x = (double*)malloc_s(N * sizeof(double));
+    double* x = newArray1d(N);
 
     for (int k = 0; k < N; k++)
     {
-        max[k] = 0;
-        for (int i = 0; i < N; i++)
+        int pivot = k;
+        for (int i = k; i < N; i++) if(fabs(Ab[pivot][k]) < fabs(Ab[i][k]))
         {
-            int flag = 1;
-            for (int j = 0; j < k; j++)
-            {
-                if (i == max[j])
-                {
-                    flag = 0;
-                }
-            }
-            if (flag)
-            {
-                max[k] = (fabs(Ab[max[k]][k]) < fabs(Ab[i][k])) ? i : max[k];
-            }
+            pivot = i;
         }
-        if (fabs(Ab[max[k]][k]) < FLOAT_ZERO_LIM)
+        double *temp = Ab[k];
+        Ab[k] = Ab[pivot];
+        Ab[pivot] = temp;
+        if (fabs(Ab[k][k]) < FLOAT_ZERO_LIM)
         {
             return NULL;
         }
 
-        for (int i = 0; i < N; i++)
+        for(int j = k + 1; j <= N; j++)
         {
-            if (i != max[k])
+            Ab[k][j] /= Ab[k][k];
+            for(int i = 0; i < N; i++) if(i != k)
             {
-                Ab[i][k] = Ab[i][k] / Ab[max[k]][k];
-                for (int j = k + 1; j < N + 1; j++)
-                {
-                    Ab[i][j] -= Ab[i][k] * Ab[max[k]][j];
-                }
+                Ab[i][j] -= Ab[i][k] * Ab[k][j];
             }
-
         }
     }
 
     for (int k = 0; k < N; k++)
     {
-        x[k] = Ab[max[k]][N] / Ab[max[k]][k];
+        x[k] = Ab[k][N];
     }
     
-    free(max);
     delArray2d(Ab, N);
     return x;
 }
