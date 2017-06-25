75379b50bf96a1e47b138f7eb128614e7629a7f6
OliverLew <oliver_lew@outlook.com>
2017-06-25 13:14:59 +0800

    reconstruct SODE functions

--- ODE/SODERKF.c
+++ ODE/SODERKF.c
@@ -1,105 +1,75 @@
+/** @file SODERKF.c */
 #include <stdio.h>
 #include <math.h>
 #include "NR.h"
 #include "constants.h"
 #include "NRprivate.h"
 
-/**
- * @brief RKF method to solve a system of ODEs
- * @param f a pointer to right-function returning an array of derivatives
- * @param y0 an array of initial values
- * @param a interval
- * @param b 
- * @param m number of equations
- * @param h0 the initial step size
- * @param TOL the required tolerance
- * @param hmax  the minimum and maximum step size allowed
- * @param hmin
- * @param n  temporarily use this number to indicate which method to use
- * @returns struct type SODEsol
- * @note remember to dispose the returned struct by DisposeSODEsol
- * @see DisposeSODEsol
- * @see SODEsol
- */
-SODEsol SODERKF(double *(*f)(double, double*), double *y0,
-     double a, double b, int m, double h0, double TOL, double hmax, double hmin, int n)
+static SODEsol SODERKF(double *(*f)(double, double*), double *y0,
+     double a, double b, int m, double h, double TOL, double hmax, double hmin, int n)
 {
-    //TODO:Integrate backwards: if a > b then integrate from b to a
-    //TODO:make the TOL a array
+    const double *A, *B, *Bstar, *C;
+    if(n == 13)
+        A = A78; B = B78; Bstar = Bstar78; C = C78;
+    else if(n == 6)
+        A = A45; B = B45; Bstar = Bstar45; C = C45;
+
     if (a > b)
     {
         fprintf(stderr, "the integration region (a,b) not legal\n");
         exit(1);
     }
-
-    long length = 1000; //length of the result, increase by 1000 every time
-    long step = 0;
-    int TOLflag; // to record if all the residuals are smaller than corresponding TOL
-    double h = h0;
-    double T = a;
-    double *delta = newArray1d(m); // a variable related to the ratio of residuals and TOL
+    int length = 1024;
+    int step = 0;
+    double *delta = newArray1d(m);
     double *w = newArray1d(m); // a vector for temporary use
     double *R = newArray1d(m); // the residuals
-    double **k = newArray2d(n, m); // the internal variables
-    double *t = newArray1d(length); // the values of variable
-    double **y = (double**)malloc_s(length * sizeof(double*)); // the values of the functions
-    y[0] = newArray1d(m);
+    double **k = (double**)malloc_s(n * sizeof(double*));
 
-    // initialization
-    t[0] = T;
+    double *t = newArray1d(length);
+    double **y = (double**)malloc_s(length * sizeof(double*));
+    /* initialization */
+    t[0] = a;
+    y[0] = newArray1d(m);
     for(int i = 0; i < m; i++)
     {
-        w[i] = y0[i];
-        y[0][i] = w[i];
+        y[0][i] = y0[i];
     }
 
-    while(T < b)
+    while(t[step] < b)
     {
-    	// calculate Ks
-        // j, n is for each k variable
-        // first we calculate the jth vectork
-        for(int j = 0; j < n; j++)
+    	/* calculate Ks */
+        for(int in = 0; in < n; in++)
         {
-            // the temporary vector parameter w to be passed to functions
-            // w = sum( a[j][n]*vectork[n], {n,0,j} )
-            for(int icomponent = 0; icomponent < m; icomponent++)
+            for(int im = 0; im < m; im++)
             {
-                w[icomponent] = y[step][icomponent];
-                for(int indexofks = 0; indexofks < j; indexofks++)
+                w[im] = y[step][im];
+                for(int j = 0; j < in; j++)
                 {
-                    w[icomponent] += A78[j * n + indexofks] * k[indexofks][icomponent];
+                    w[im] += A[in * n + j] * k[j][im] * h;
                 }
             }
-            // i, m is for each component of a variable, the same number as the number of ODEs
-            double *temp = f(T + C78[j] * h, w);
-            for(int i = 0; i < m; i++)
-            {
-                // the ith component of vector_k corresponding to the ith function
-                // The_jth_vector_k[i] = h*ith_function(t, w)
-                k[j][i] = h * temp[i];
-            }
+            k[in] = f(t[step] + C[in] * h, w);
         }
 
-        TOLflag = 0;
-        // calculate the residual R
-        for(int icomponent = 0; icomponent < m; icomponent++)
+        int TOLflag = 0; /* to record if all the residuals are smaller than corresponding TOL */
+        /* calculate the residual R */
+        for(int im = 0; im < m; im++)
         {
-            R[icomponent] = 0;
-            for(int indexofks = 0; indexofks < n; indexofks++)
+            R[im] = 0;
+            for(int in = 0; in < n; in++)
             {
-                R[icomponent] += (B78[indexofks] - Bstar78[indexofks]) * k[indexofks][icomponent] / h;
+                R[im] += (B[in] - Bstar[in]) * k[in][im];
             }
+            delta[im] = pow(TOL / fabs(R[im]) / 2, 1.0 / 7);
 
-            delta[icomponent] = pow(TOL / R[icomponent] / 2, 1.0 / 7);
-
-            // lower the step length if the accuracy is not enough
-            if(R[icomponent] > TOL)
+            /* lower the step length if the accuracy is not enough */
+            if(R[im] > TOL)
             {
-                h = h * delta[icomponent];
-                // printf("The R is %lf, changing step length: %lf\n",R[icomponent], h);
+                h *= delta[im] <= 0.1 ? 0.1 : (delta[im] >= 1.0 ? 1.0 : delta[im]);
                 if(h < hmin)
                 {
-                    fprintf(stderr, "T = %lf, minimal limit exceeds! lower minimal limit required.\n", T);
+                    fprintf(stderr, "T = %lf, minimal limit exceeds! lower minimal limit required.\n", t[step]);
                     exit(1);
                 }
                 TOLflag = 1;
@@ -107,47 +77,36 @@ SODEsol SODERKF(double *(*f)(double, double*), double *y0,
             }
         }
 
-        // the residuals are all smaller than the TOL, so record the result to array
+        /* the residuals are all smaller than the TOL, so record the result to array */
         if(TOLflag == 0)
         {
-            // lengthen the array if the current space is not enough.
-            if((step + 1) == length)
+            step++;
+            /* lengthen the array if the current space is not enough. */
+            if(step == length)
             {
-                length += 1000;
+                length += 1024;
                 t = (double*)realloc_s(t, length * sizeof(double));
                 y = (double**)realloc_s(y, length * sizeof(double*));
             }
-            // allocate a new line in y.
-            y[step+1] = (double*)malloc_s(m * sizeof(double));
-            // calculate the new numbers
-            for(int icomponent = 0; icomponent < m; icomponent++)
+            y[step] = newArray1d(m);
+            /* calculate the new numbers */
+            for(int im = 0; im < m; im++)
             {
-                w[icomponent] = y[step][icomponent];
-                for(int indexofks = 0; indexofks < n; indexofks++)
+                y[step][im] = y[step - 1][im];
+                for(int in = 0; in < n; in++)
                 {
-                    w[icomponent] += B78[indexofks] * k[indexofks][icomponent];
-                }
-                y[step+1][icomponent] = w[icomponent];
-            }
-
-            // increase the time t by interval h
-            T += h;
-            step++;
-            t[step] = T;
-
-            double mindelta = 1.5;
-            // raise the step length to a suitable value since all the R are below TOL
-            for(int icomponent = 0; icomponent < m; icomponent++)
-            {
-                if(delta[icomponent] < mindelta)
-                {
-                    mindelta = delta[icomponent];
+                    y[step][im] += B[in] * k[in][im] * h;
                 }
             }
-            if(mindelta > 1.5)
+            t[step] = t[step - 1] + h;
+
+            double mindelta = 1;
+            for(int im = 0; im < m; im++) if(delta[im] < mindelta)
             {
-                h = h * mindelta;
+                mindelta = delta[im];
             }
+            h *= mindelta <= 1.0 ? 1.0 : (mindelta >= 2.0 ? 2.0 : mindelta);
+            h = h > hmax ? hmax : h;
         }
     }
 
@@ -155,11 +114,57 @@ SODEsol SODERKF(double *(*f)(double, double*), double *y0,
     delArray1d(delta);
     delArray1d(w);
     delArray1d(R);
-    delArray2d(k, n);
+    free(k);
 
     // resize the arrays to suitable size -- number of steps
     y = (double**)realloc_s(y, step * sizeof(double*));
-    t = (double*)realloc_s(t, step * sizeof(double));
+    t = Array1dResize(t, step);
     SODEsol sol = newSODEsol(step, t, y);
     return sol;
+}
+/**
+ * @brief RKF method to solve a system of ODEs
+ * @param f a pointer to right-function returning an array of derivatives
+ * @param y0 an array of initial values
+ * @param a lower limit of interval
+ * @param b upper limit of interval
+ * @param m number of equations
+ * @param h the initial step size
+ * @param TOL the required tolerance
+ * @param hmax the minimum step size allowed
+ * @param hmin the maximum step size allowed
+ * @returns struct type SODEsol
+ * @note remember to dispose the returned struct by DisposeSODEsol
+ * @see DisposeSODEsol
+ * @see SODEsol
+ * @todo Integrate backwards: if a > b then integrate from b to a
+ * @todo make the TOL a array
+ */
+SODEsol SODERKF45(double *(*f)(double, double*), double *y0,
+     double a, double b, int m, double h, double TOL, double hmax, double hmin)
+{
+    return SODERKF(f, y0, a, b, m, h, TOL, hmax, hmin, 6);
+}
+/**
+ * @brief RKF method to solve a system of ODEs
+ * @param f a pointer to right-function returning an array of derivatives
+ * @param y0 an array of initial values
+ * @param a lower limit of interval
+ * @param b upper limit of interval
+ * @param m number of equations
+ * @param h the initial step size
+ * @param TOL the required tolerance
+ * @param hmax the minimum step size allowed
+ * @param hmin the maximum step size allowed
+ * @returns struct type SODEsol
+ * @note remember to dispose the returned struct by DisposeSODEsol
+ * @see DisposeSODEsol
+ * @see SODEsol
+ * @todo Integrate backwards: if a > b then integrate from b to a
+ * @todo make the TOL a array
+ */
+SODEsol SODERKF78(double *(*f)(double, double*), double *y0,
+     double a, double b, int m, double h, double TOL, double hmax, double hmin)
+{
+    return SODERKF(f, y0, a, b, m, h, TOL, hmax, hmin, 13);
 }
--- ODE/SODERungeKutta.c
+++ ODE/SODERungeKutta.c
@@ -1,22 +1,25 @@
+/** @file SODERungeKutta.c */
 #include "NR.h"
 #include "NRprivate.h"
 /**
  * @brief Classic Runge-Kutta Method to solve a System of ODEs.
+ * @param f a pointer to an array of functions.
+ * @param a lower limit of interval
+ * @param b upper limit of interval
+ * @param y0 the initial values,
  * @param m the number of ODEs,
  * @param N the numebr of steps,
- * @param (a, b) the interval,
- * @param y0 the initial values,
- * @param f point to an array of functions.
- * @return a 2D array of values of all functions in all steps.
+ * @return an ::SODEsol variable
  */
-double **SODERungeKutta(double (**f)(double, double*), double a, double b, double *y0, int m, int N)
+SODEsol SODERungeKutta(double *(*f)(double, double*), double a, double b, double *y0, int m, int N)
 {
     double h = (b - a) / N;
-    double t = a;
+    double *t = newArray1d(N + 1);
     double *w = newArray1d(m);
     double **y = newArray2d(N + 1, m);
-    double **k = newArray2d(4, m);
+    double **k = (double**)malloc_s(4 * sizeof(double*));
 
+    t[0] = a;
     for(int i = 0; i < m; i++)
     {
         w[i] = y0[i];
@@ -24,31 +27,40 @@ double **SODERungeKutta(double (**f)(double, double*), double a, double b, doubl
     }
     for(int i = 0; i < N; i++)
     {
+        k[0] = f(t[i], w);
+
         for(int j = 0; j < m; j++)
         {
-            k[0][j] = h * f[j](t, w);
-            w[j] = y[i][j] + k[0][j] / 2;
+            w[j] = y[i][j] + k[0][j] * h / 2;
         }
+        k[1] = f(t[i] + h / 2, w);
+
         for(int j = 0; j < m; j++)
         {
-            k[1][j] = h * f[j](t + h / 2, w);
-            w[j] = y[i][j] + k[1][j] / 2;
+            w[j] = y[i][j] + k[1][j] * h / 2;
         }
+        k[2] = f(t[i] + h / 2, w);
+
         for(int j = 0; j < m; j++)
         {
-            k[2][j] = h * f[j](t + h / 2, w);
-            w[j] = y[i][j] + k[2][j];
+            w[j] = y[i][j] + k[2][j] * h;
         }
+        k[3] = f(t[i] + h, w);
+
         for(int j = 0; j < m; j++)
         {
-            k[3][j] = h * f[j](t + h, w);
-            y[i + 1][j] = y[i][j] + (k[0][j] + k[1][j] * 2 + k[2][j] * 2 + k[3][j]) / 6;
+            y[i + 1][j] = y[i][j] + (k[0][j] + k[1][j] * 2 + k[2][j] * 2 + k[3][j]) * h / 6;
         }
-        t += h;
+
+        t[i + 1] = t[i] + h;
+        delArray1d(k[0]);
+        delArray1d(k[1]);
+        delArray1d(k[2]);
+        delArray1d(k[3]);
     }
     delArray1d(w);
-    delArray2d(4, N);
-    return y;
+    free(k);
+    return newSODEsol(N + 1, t, y);
 }
 
 
